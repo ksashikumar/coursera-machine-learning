92d90fab343b3dc7675a45868a0ea9423ea85fca
ksashikumar <ksashikumark93@gmail.com>
2016-02-15 00:02:37 +0530

    finish assignment ex2

--- assignments/ex2/costFunction.m
+++ assignments/ex2/costFunction.m
@@ -1,32 +1,18 @@
 function [J, grad] = costFunction(theta, X, y)
-%COSTFUNCTION Compute cost and gradient for logistic regression
-%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
-%   parameter for logistic regression and the gradient of the cost
-%   w.r.t. to the parameters.
 
-% Initialize some useful values
-m = length(y); % number of training examples
+m = length(y);
 
-% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
+hx =  sigmoid(X*theta);
 
+cost1 = -y' * log(hx);
 
+cost2 = (1 - y') * log(1 - hx);
 
+J = (1 / m) * (cost1 - cost2);
 
-
-
-
-
-% =============================================================
+grad = (1 / m) * ((hx - y)' * X);
 
 end
--- assignments/ex2/costFunctionReg.m
+++ assignments/ex2/costFunctionReg.m
@@ -1,27 +1,26 @@
 function [J, grad] = costFunctionReg(theta, X, y, lambda)
-%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
-%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
-%   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+m = length(y);
 
-% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+hx =  sigmoid(X*theta);
 
+cost1 = -y' * log(hx);
 
+cost2 = (1 - y') * log(1 - hx);
 
+thetaZero = theta;
 
+thetaZero(1) = 0;
 
+lambaJ = (lambda / (2 * m)) * sum(thetaZero .^ 2);
 
-% =============================================================
+lambdaG = lambda / m * thetaZero;
+
+J = (1 / m) * (cost1 - cost2) + lambaJ;
+
+grad = ((1/m) * (X' * (hx - y))) + lambdaG;
 
 end
